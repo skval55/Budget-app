b11462d7af6cf1dd51e4c5600c40146bb68d869e
Samuel Valadez <skvaladez@gmail.com>
2026-03-31 11:26:36 -0600

    gottem

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,26 @@
 import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
-    import "./tailwind.css";
-    import   "./style.css";
-  export default function App() {
-    return (
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <link href="/src/style.css" rel="stylesheet" />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Outlet />
-  
-          <Scripts />
-        </body>
-      </html>
-    );
-  }
-  
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+} from "@remix-run/react";
+import "./tailwind.css";
+import "./style.css";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
